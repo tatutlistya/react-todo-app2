8d8ee03c44bfd34f27aceb6f60615a642d0a12df
tatutlistya <tatutlistya2405@gmail.com>
2024-06-09 00:28:02 +0700

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,24 @@ function App() {
 
   console.log(todos)
 
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
+  toggleCompleted = (todoId) => {
+    console.log(todoId)
+  }
+  
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   )
 }
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,8 +1,19 @@
-const Todos = ({ todos }) => {
+import React from 'react'
+import TodoItem from './TodoItem'
+
+
+const Todos = ({ todos, toggleCompleted }) => {
   return (
     <div style={styles.container}>
       {todos.map((todo) => {
-        return <TodoItem key={todo.id} todo={todo} />
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+      
+            toggleCompleted={toggleCompleted}
+          />
+        )
       })}
     </div>
   )
