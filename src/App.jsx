25585ddfe0958d7541375a09158e2824d3560961
tatutlistya <tatutlistya2405@gmail.com>
2024-06-09 11:33:55 +0700

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState, createContext } from 'react'
-import Todos from './components/Todos'
-import TodoForm from './components/TodoForm'
+import React, { useState, createContext, useEffect } from 'react'
+import Todos from './components/Todos';
+import TodoForm from './components/TodoForm';
 
-export const TodoContext = createContext()
+export const TodoContext = createContext();
 
 function App() {
   const [todos, setTodos] = useState([
@@ -21,24 +21,29 @@ function App() {
       title: 'Study React with Ninja Ken',
       completed: false,
     },
+    {
+      id: 4,
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, itaque dicta exercitationem officiis ipsam optio? Temporibus incidunt, illum odio distinctio quia quae aliquid officiis adipisci saepe magnam ex veniam doloremque.',
+      completed: false,
+    },
   ])
-
-
-  console.log(todos)
 
   const toggleCompleted = (todoId) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === todoId) {
+        console.log(todo);
         todo.completed = !todo.completed
       }
       return todo
     })
-    setTodos(updatedTodos)
+    setTodos(updatedTodos);
   }
 
-  toggleCompleted = (todoId) => {
-    console.log(todoId)
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(updatedTodos);
   }
+
   const addTodo = (todoTitle) => {
     if (todoTitle === '') {
       return
@@ -50,14 +55,17 @@ function App() {
       completed: false,
     }
 
-    const updatedTodos = todos.concat(newTodo)
-    setTodos(updatedTodos)
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
   }
 
   return (
-    <TodoContext.Provider value={{ toggleCompleted, deleteTodo }}>
-      <div style={styles.container}>
-        <h1 style={styles.title}>My Todo List</h1>
+    <TodoContext.Provider value={{
+      toggleCompleted,
+      deleteTodo
+    }}>
+      <div className='container text-center p-3 mx-auto h-full'>
+        <h1 className='font-bold font-sans text-3xl text-blue-600'>My Todo List</h1>
         <TodoForm addTodo={addTodo} />
         <Todos todos={todos} />
       </div>
